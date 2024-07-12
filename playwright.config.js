45596d63c7df89378a23487ef0c7f0e80004a591
DenysKulikov <deniska2002kulikov@gmail.com>
2024-07-12 14:43:00 +0300

    chenged yml

--- playwright.config.js
+++ playwright.config.js
@@ -21,12 +21,12 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['list'], ['allure-playwright']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
-    headless: false, // Set to true if you want to run tests in headless mode
+    headless: true, // Set to true if you want to run tests in headless mode
     baseURL: 'https://www.redmine.org',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
@@ -78,4 +78,3 @@ module.exports = defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
